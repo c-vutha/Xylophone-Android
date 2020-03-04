f02c06f184d858700b3253d56fd0eec37e5878a7
c-vutha <chhuonvutha46@gmail.com>
2020-03-04 14:36:54 +0700

    music app

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -1,9 +1,11 @@
 package com.londonappbrewery.xylophonepm;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool mSoundPool;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +34,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
-
+        final Context context = getApplicationContext();
+        mCSoundId = mSoundPool.load(context, R.raw.note1_c, PRIORITY);
+        mDSoundId = mSoundPool.load(context, R.raw.note2_d, PRIORITY);
+        mESoundId = mSoundPool.load(context, R.raw.note3_e, PRIORITY);
+        mFSoundId = mSoundPool.load(context, R.raw.note4_f, PRIORITY);
+        mGSoundId = mSoundPool.load(context, R.raw.note5_g, PRIORITY);
+        mASoundId = mSoundPool.load(context, R.raw.note6_a, PRIORITY);
+        mBSoundId = mSoundPool.load(context, R.raw.note7_b, PRIORITY);
 
     }
 
     // TODO: Add the play methods triggered by the buttons
+    public void playC(View view) {
+        Log.d("Xylophone", "Play C Key");
+        playNote(mCSoundId);
+    }
 
 
+    public void playD(View view) {
+        Log.d("Xylophone", "Play D Key");
+        playNote(mDSoundId);
+    }
 
+    public void playE(View view) {
+        Log.d("Xylophone", "Play E Key");
+        playNote(mESoundId);
+    }
+
+    public void playF(View view) {
+        Log.d("Xylophone", "Play F Key");
+        playNote(mFSoundId);
+    }
+
+    public void playG(View view) {
+        Log.d("Xylophone", "Play G Key");
+        playNote(mGSoundId);
+    }
+
+    public void playA(View view) {
+        Log.d("Xylophone", "Play A Key");
+        playNote(mASoundId);
+    }
+
+    public void playB(View view) {
+        Log.d("Xylophone", "Play B Key");
+        playNote(mBSoundId);
+    }
+
+    private int playNote(int noteSoundId) {
+        return mSoundPool.play(noteSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
 }
